990b298fa154b5f0a6b588096e3998194449a9dc
Akaeda <anonym.nobody13@gmail.com>
2018-06-22 19:29:34 +0200

    Add files via upload
    Hab die Farben meiner Seite den anderen Websiten angepasst.

---
+++ MEDT Part/BackupMagic-Websetie Color Changed/php/index.php
@@ -0,0 +1,11 @@
+ <?php
+ 
+$username = "root";
+$password = "";
+$database = "db_test";
+
+// Create connection
+$database = new mysqli('localhost', $username, $password, $database) or die();
+
+
+?> 
